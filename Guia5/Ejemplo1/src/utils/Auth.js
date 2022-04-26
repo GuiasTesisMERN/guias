6a7055fd1b308f684367cc4a4b969ac64adf1752
andr497 <andrangel97@gmail.com>
2022-04-25 22:17:26 -0600

    Validacion de token agregada

--- Guia5/Ejemplo1/src/utils/Auth.js
+++ Guia5/Ejemplo1/src/utils/Auth.js
@@ -5,7 +5,7 @@ const { UnAuthorizedError } = require('./app-errors');
 
 module.exports = {
     /**
-     * 
+     * Funcion para crear token en base al email del usuario
      * @param {string} email 
      * @returns 
      */
@@ -13,15 +13,30 @@ module.exports = {
         return jwt.sign({email}, APP_SECRET, { expiresIn: '30s' })
     },
 
+    /**
+     * Funcion para validar token
+     * @param {Request} req 
+     * @returns 
+     */
     validateTokenSignature: async(req) => {
         const authHeader = req.headers['authorization'];
-        const token = authHeader && authHeader.split(' ')[1];
 
-        if(token === null) {
-            throw new UnAuthorizedError('No se encontró un token válido en la petición');
+        if(typeof authHeader === 'undefined') {
+            throw new UnAuthorizedError('Authorization Header es requerido en la app');
         }
 
-        const payload = await jwt.verify(token, APP_SECRET);
+        const token = authHeader.split(' ')[1];
+
+        if(token === "undefined") {
+            throw new UnAuthorizedError('Token inválido');
+        }
+
+        const payload = await jwt.verify(token, APP_SECRET, (err) => {
+            if(err) {
+                console.error(err.expiredAt)
+                throw new UnAuthorizedError(err.message)
+            }
+        });
         
         return payload;
     }
