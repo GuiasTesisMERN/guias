3fd6d06014765cf0e8f77b78e9941915121e56fc
jose <andrangel97@gmail.com>
2022-10-27 15:27:58 -0600

    Guia 11 agregando ejemplo 1

--- Guia11/ejemplo1/frontend/src/App.js
+++ Guia11/ejemplo1/frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
@@ -10,32 +10,23 @@ import PrivateRoute from "./components/PrivateRoute";
 import PublicRoute from "./components/PublicRoute";
 
 import Layout from "./components/Layout/Layout";
+import ListaUsuarios from "./pages/ListaUsuarios";
 
 function App() {
+
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
           <Route path="/" element={<PrivateRoute><Bienvenido/></PrivateRoute>} />
+          <Route path="/listado_usuarios" element={<PrivateRoute><ListaUsuarios /></PrivateRoute>} />
           <Route path="/login" element={<PublicRoute><Login/></PublicRoute>} />
           <Route path="/registrarse" element={<Registrarse/>} />
-          <Route path="/lista/:id" element={<ComponentePrueba/>} />
           <Route path="*" element={<NotFound/>} />
         </Routes>
       </Layout>
     </BrowserRouter>
   );
-}
-
-function ComponentePrueba() {
-
-  const { id } = useParams();
-
-  return (
-    <p>
-      Este es el valor del parámetro (id) {id}
-    </p>
-  )
 }
 
 export default App;
